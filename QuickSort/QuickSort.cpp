9f357f6684caaf8e048bcbca77183c51efdebe6c
dinaamlll <dinamaliaa17@gmail.com>
2023-03-31 12:13:53 -0800

    memasukkan input

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -99,3 +99,12 @@ void display() {
 	cout << "Number of data movemen: " << mov_count << endl;
 }
 
+int main() {
+	input();
+	//sort the array using quick sort
+	q_short(0, n - 1);
+	display();
+	system("pause");
+
+	return 0;
+}
