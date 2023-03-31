31d71e093246b094ba484a6628e0d8b1932876f0
dinaamlll <dinamaliaa17@gmail.com>
2023-03-31 11:17:49 -0800

    memasukkan for

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -22,3 +22,10 @@ void input() {
 	cout << "\n========================" << endl;
 	cout << "\nEnter attau element" << endl;
 	cout << "\n========================" << endl;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << ">";
+		cin >> arr[i];
+	}
+}
